c3c55cb53fda4bd98f5d50cf3300816c80534bc6
glasvisax <1icyscarecrow@gmail.com>
2024-07-16 22:25:40 +0300

    init commit

---
+++ LocalPaths.cmake
@@ -0,0 +1,4 @@
+set(THOR_MODULE D:/Thor/install/cmake/Modules CACHE PATH "Path to Thor Module")
+set(THOR_INCLUDE_DIR CACHE PATH D:/Thor/install/include CACHE PATH "Path to Thore headers")
+set(SFML_DIR D:/SFML/install/lib/cmake/SFML CACHE PATH "Path to SFML Module")
+set(GTest_DIR D:/GoogleTest/install/lib/cmake/GTest CACHE PATH "Path to GTest Module")
